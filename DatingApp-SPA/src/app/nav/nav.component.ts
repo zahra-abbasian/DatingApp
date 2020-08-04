369374b9497e87e08c811c9d7a9b5575bc7b599e
Zahra Abbasian <zahra@192-168-1-12.tpgi.com.au>
2020-08-04 16:59:06 +1000

    End of section 6

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,36 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-
+import { AlertifyService } from '../_services/alertify.service';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  //an empty object for entring in username and password fields
+  // an empty object for entring in username and password fields
   model: any = {};
 
-  constructor(private authServise: AuthService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService) { }
 
+  // tslint:disable-next-line: typedef
   ngOnInit() {
   }
 
+  // tslint:disable-next-line: typedef
   login() {
-    this.authServise.login(this.model).subscribe(next => {
-      console.log('Logged in successfully');
+    this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('Logged in successfully');
     }, error => {
-      console.log('Failed to login');
+      this.alertify.error('Failed to login');
     });
   }
 
+  // tslint:disable-next-line: typedef
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    return this.authService.loggedIn();
   }
 
+  // tslint:disable-next-line: typedef
   logout() {
     localStorage.removeItem('token');
-    console.log('Logged Out');
+    this.alertify.message('Logged Out');
   }
 
 }
